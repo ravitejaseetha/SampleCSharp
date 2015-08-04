96e25f3045ab93a80ee62ee12b03ccd8a29e8221
raviteja <ravitejaseetha@gmail.com>
2015-08-04 14:56:21 +0530

    Palindrome example

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -10,6 +10,22 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
+            string s, rev = "";
+            Console.WriteLine("Enter string");
+            s = Console.ReadLine();
+            for (int i = s.Length-1; i >=0; i--)
+            {
+                rev += s[i].ToString();
+            }
+            if (rev == s)
+            {
+                Console.WriteLine("String is palindrome\nEntered string is {0} and reverse of the string is {1}",s,rev);
+            }
+            else
+            {
+                Console.WriteLine("Entererd string is not a palindrome");
+            }
+            Console.ReadKey();
         }
     }
 }
