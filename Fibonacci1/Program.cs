dcccf43364cf29ab8b445c1e4800cae5850154f7
raviteja <ravitejaseetha@gmail.com>
2015-08-04 14:48:09 +0530

    sample programs

---
+++ Fibonacci1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           fib(9);
+            Console.WriteLine("nth fibonacci number {0}", GetFib(4));
+            Console.ReadKey();
+        }
+
+        public static void fib(int len)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.WriteLine("{0} {1}",a,b);
+            for (int i = 2; i < len; i++)
+            {
+                c = a + b;
+                Console.WriteLine("{0}",c);
+                a = b;
+                b = c;
+            }
+        }
+
+        public static int GetFib(int num)
+        {
+            int number = num - 1;
+            int[] fib = new int[number + 1];
+            fib[0] = 0;
+            fib[1] = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                fib[i] = fib[i - 2] + fib[i - 1];
+            }
+            return fib[number];
+        }
+    }
+   
+}
---
+++ SampleExamples/Encapsulation1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleExamples
+{
+     class Encapsulation1
+    {
+        private string accountName;
+
+        public string GetAccount()
+        {
+            return accountName;
+        }
+
+        public void SetAccount(string name)
+        {
+            accountName = name;
+        }
+
+
+        private static void Main(string[] args)
+        {
+
+            Encapsulation1 pr = new Encapsulation1();
+            pr.SetAccount("hello account");
+            Console.WriteLine(pr.GetAccount());
+            Console.ReadKey();
+
+        }
+
+    }
+}
+
